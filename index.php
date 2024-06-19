c6fda56d687e44f7d0570abb15d8f6cfd2846b32
RezaHaykal <168186039+RezaHaykal@users.noreply.github.com>
2024-06-19 22:08:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<center>Username: " . $_POST['username'] . "</center><br>";
+echo "<center>Email: " . $_POST['email'] . "</center><br>";
+echo "<center>Password: " . $_POST['password'] . "</center><br>";
+?>
